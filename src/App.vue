ce2c02a05996239a58c0abe4c7f8233eadb19d00
ChangJoo Park <pcjpcj2@gmail.com>
2017-01-29 13:01:51 +0900

    TodoMVC 템플릿 추가

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <todos></todos>
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos'
+
 export default {
   name: 'app',
   components: {
+    Todos
   }
 }
 </script>
