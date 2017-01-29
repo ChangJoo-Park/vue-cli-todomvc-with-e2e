d5237197cebd1aa8000d009d904d040f5d882027
ChangJoo Park <pcjpcj2@gmail.com>
2017-01-29 16:05:20 +0900

    기본 Todo 데이터 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,19 @@
 const STORAGE_KEY = 'todos-vuejs'
 const localStorage = window.localStorage
-
+const defaultTodo = [
+  { completed: false, title: 'Make First Vue.js App' },
+  { completed: true, title: 'Buy The Majesty of Vue 2' },
+  { completed: false, title: 'Join the Vue.js Slack' },
+  { completed: false, title: 'Join the Vue.js Facebook group' }
+]
 module.exports = {
   fetch () {
-    return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    let todos = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    if (!todos) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(defaultTodo))
+      todos = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    }
+    return todos
   },
   save (todos) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
